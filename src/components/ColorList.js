78c27261fcc38c447d75532a79ed3f106cc7da5e
auma2022 <auma.amwayi@student.moringaschool.com>
2022-07-20 13:29:20 +0300

    add comments

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+/* import the `color` module
+2. create a color list, with 4 colors
+3. create a div with a `h1` with the title `Top 5 CSS Colors`
+4. render a list, with 4 html list items (one for each color)
+5. style each list item, through the use of styled-components
+6. add a `color` property to the styled-components props
+7. pass the color prop to each styled-components styled li element
+8. render the styled-components DOM tree*/
 function ColorList() {
   const colors = [
     "firebrick",
